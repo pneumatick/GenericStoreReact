6997b7eac81ca5d3fa8b67e93b3ce73c6a1968d5
pneumatick <jhwwefan@gmail.com>
2022-11-16 12:23:27 -0500

    Store page functionality fully converted from previous format to a React-compatible one

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -8,10 +8,30 @@ export default class Store extends React.Component {
         };
     }
 
+    async getProducts() {
+        let newProducts = [];
+
+        await fetch('http://localhost:8080/store/products')
+        .then((response) => response.json())
+        .then((products) => {
+            products.forEach((product, id) => {
+                let productListing = <p key={id}>{product.product}: ${product.price} ({product.quantity} available)</p>;
+                newProducts.push(productListing);
+            });
+        });
+
+        this.setState({ products: newProducts });
+    }
+
+    componentDidMount() {
+        this.getProducts();
+    }
+
     render() {
         return (
             <div>
                 <h1>Store</h1>
+                {this.state.products}
             </div>
         );
     }
